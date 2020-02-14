84cb049a12a2abfab16456cbda4fe28e2d2cf01c
Rabin Thapa <rbeen.mgr@gmail.com>
2020-02-14 20:24:31 +0545

    first commit

---
+++ Python basics.py
@@ -0,0 +1,61 @@
+#printing a statement
+
+print (" Hello Group B")
+
+#---------------------------------------
+
+
+#variables && strings
+
+a=123
+b=1.23
+c="hello"
+print(a,b,c)
+
+
+#checking the type of variable
+
+print(type(a),type(b),type(c))
+
+
+#assigning variables in same line!!!
+
+a,b=2,"python"
+
+print(a,b)
+#--------------------------------------
+#lists
+
+list=[1,2,3,4,5,6]
+print(list)
+
+#lists can contail lists of lists...!!!
+
+a=[[1,2],[3,4],[5,6]]
+print(a)
+
+print(a[1])
+
+#using ''+'' operator in list!!
+
+b=[1,2,3]
+c=[2,3,4]
+print(b + c)
+
+#addition in lists!!!
+
+print(b[1] + c[0])
+
+#--------------------------------------
+
+#operators
+
+print(1+2)     #addition
+print(10/5)    #division
+print(5-2)     #subraction
+print(2*8)     #multiplication
+print(10%4)    #modulas(remainder)
+print(2**2)    #square
+print(3**3)    #cubic
+
+print ("Group B  "* 10)
